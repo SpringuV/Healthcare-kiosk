772fa2897ac612d37b0c4fdd62fc61715bfc499d
xuanvu <xuanvuaudi2002@gmail.com>
2025-08-25 21:01:44 +0700

    fix lite

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,8 @@ import InputCCCD from './components/Modal/inputCCCD'
 import NonInsurrance from './components/Modal/non-insurrance'
 import InfoInsurrance from './components/Modal/insurrance_info'
 import NonInsurranceInfo from './components/Modal/non-insurrance-info'
-import { InsurranceProvider, useInsurrance } from './components/context/insurrance_context'
+import {useInsurrance } from './components/context/insurrance_context'
 import RegisterSuccess from './components/Modal/register-success'
-import { ServiceProvider } from './components/context/service_context'
 import UpdateInfoPatientInsurrance from './components/Modal/update_insurrance_info'
 import LayoutDefault from './components/Layout/LayoutDefault'
 import HomePage from './components/Modal/homepage'
@@ -21,41 +20,37 @@ function App() {
     const { setFormData } = useForm()
     const { setInsurranceInfo } = useInsurrance()
     return (
-        <InsurranceProvider>
-            <ServiceProvider>
-                <Routes>
-                    <Route path='/' element={<LayoutDefault></LayoutDefault>}>
-                        <Route index element={<HomePage></HomePage>}></Route>
-                        <Route path='insur' element={<InputCCCD mode="insurance" onClose={() => navigate('/')}
-                            onSuccess={(data) => {
-                                setInsurranceInfo(data);
-                                navigate('/insur/info');
-                            }}></InputCCCD>}>
-                            <Route path='info' element={<InfoInsurrance onClose={() => navigate('/service')}></InfoInsurrance>}></Route>
-                            <Route path='update-info' element={<UpdateInfoPatientInsurrance></UpdateInfoPatientInsurrance>}></Route>
-                        </Route>
-                        <Route path='result-search' element={<InputCCCD mode="history" onClose={() => navigate(-1)}
-                            onSuccess={(data) => {
-                                navigate('/result-search/result', { state: data });
-                            }}></InputCCCD>}>
-                            <Route path='result' element={<ResultSearch onClose={() => navigate(-1)}></ResultSearch>}></Route>
-                        </Route>
-                        <Route path='service' element={<Service></Service>}></Route>
-                        <Route path='non-insur' element={<InputCCCD mode="non-insurance" onClose={() => navigate('/')}
-                            onSuccess={(data) => {
-                                setFormData(data);
-                                navigate('/non-insur/info');
-                            }}></InputCCCD>}>
-                            <Route path='register' element={<NonInsurrance onClose={() => navigate('/')}></NonInsurrance>}></Route>
-                            <Route path='info' element={<NonInsurranceInfo></NonInsurranceInfo>}></Route>
-                        </Route>
-                        <Route path='confirm-registration' element={<RegisterSuccess></RegisterSuccess>}></Route>
+        <Routes>
+            <Route path='/' element={<LayoutDefault></LayoutDefault>}>
+                <Route index element={<HomePage></HomePage>}></Route>
+                <Route path='insur' element={<InputCCCD mode="insurance" onClose={() => navigate('/')}
+                    onSuccess={(data) => {
+                        setInsurranceInfo(data);
+                        navigate('/insur/info');
+                    }}></InputCCCD>}>
+                    <Route path='info' element={<InfoInsurrance onClose={() => navigate('/service')}></InfoInsurrance>}></Route>
+                    <Route path='update-info' element={<UpdateInfoPatientInsurrance></UpdateInfoPatientInsurrance>}></Route>
+                </Route>
+                <Route path='result-search' element={<InputCCCD mode="history" onClose={() => navigate(-1)}
+                    onSuccess={(data) => {
+                        navigate('/result-search/result', { state: data });
+                    }}></InputCCCD>}>
+                    <Route path='result' element={<ResultSearch onClose={() => navigate(-1)}></ResultSearch>}></Route>
+                </Route>
+                <Route path='service' element={<Service></Service>}></Route>
+                <Route path='non-insur' element={<InputCCCD mode="non-insurance" onClose={() => navigate('/')}
+                    onSuccess={(data) => {
+                        setFormData(data);
+                        navigate('/non-insur/info');
+                    }}></InputCCCD>}>
+                    <Route path='register' element={<NonInsurrance onClose={() => navigate('/')}></NonInsurrance>}></Route>
+                    <Route path='info' element={<NonInsurranceInfo></NonInsurranceInfo>}></Route>
+                </Route>
+                <Route path='confirm-registration' element={<RegisterSuccess></RegisterSuccess>}></Route>
 
-                    </Route>
-                    <Route path='payment' element={<PaymentWithQR></PaymentWithQR>}></Route>
-                </Routes>
-            </ServiceProvider>
-        </InsurranceProvider>
+            </Route>
+            <Route path='payment' element={<PaymentWithQR></PaymentWithQR>}></Route>
+        </Routes>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,16 @@ import './index.css'
 import App from './App.jsx'
 import { FormProvider } from './components/context/form_context.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { InsurranceProvider } from './components/context/insurrance_context.jsx'
+import { ServiceProvider } from './components/context/service_context.jsx'
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-        <FormProvider>
-            <App />
-        </FormProvider>
+        <InsurranceProvider>
+            <ServiceProvider>
+                <FormProvider>
+                    <App />
+                </FormProvider>
+            </ServiceProvider>
+        </InsurranceProvider>
     </BrowserRouter>
 )
