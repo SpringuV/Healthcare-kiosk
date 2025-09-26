1bf9dd939bdba4c8f64a9f300b99e913c9724201
xuanvu <xuanvuaudi2002@gmail.com>
2025-09-26 16:40:56 +0700

    dev login

--- frontend_admin_next/src/components/login.tsx
+++ frontend_admin_next/src/components/login.tsx
@@ -1,11 +1,11 @@
 'use client'
 import {
-    AlipayCircleOutlined,
+    FacebookOutlined,
+    GithubOutlined,
+    GoogleOutlined,
     LockOutlined,
     MobileOutlined,
-    TaobaoCircleOutlined,
     UserOutlined,
-    WeiboCircleOutlined,
 } from '@ant-design/icons';
 import {
     LoginForm,
@@ -15,7 +15,7 @@ import {
     ProFormText,
     setAlpha,
 } from '@ant-design/pro-components';
-import { Space, Tabs, message, theme } from 'antd';
+import { Button, Space, Tabs, message, theme } from 'antd';
 import type { CSSProperties } from 'react';
 import { useState } from 'react';
 
@@ -36,15 +36,28 @@ const LoginUI = () => {
         <ProConfigProvider hashed={false}>
             <div style={{ backgroundColor: token.colorBgContainer }}>
                 <LoginForm
+                    // Thay đổi text button đăng nhập
+                    submitter={{
+                        render: () => (
+                            <Button
+                                type="primary"
+                                htmlType="submit"
+                                size="large"
+                                style={{ width: '100%' }}
+                            >
+                                Đăng nhập vào hệ thống
+                            </Button>
+                        ),
+                    }}
                     logo="https://github.githubassets.com/favicons/favicon.png"
                     title="Github"
-                    subTitle="全球最大的代码托管平台"
+                    subTitle="The world's largest code hosting platform"
                     actions={
                         <Space>
-                            其他登录方式
-                            <AlipayCircleOutlined style={iconStyles} />
-                            <TaobaoCircleOutlined style={iconStyles} />
-                            <WeiboCircleOutlined style={iconStyles} />
+                            Đăng nhập bằng
+                            <GoogleOutlined style={iconStyles} /> 
+                            <GithubOutlined style={iconStyles} />
+                            <FacebookOutlined style={iconStyles} />
                         </Space>
                     }
                 >
@@ -53,8 +66,8 @@ const LoginUI = () => {
                         activeKey={loginType}
                         onChange={(activeKey) => setLoginType(activeKey as LoginType)}
                     >
-                        <Tabs.TabPane key={'account'} tab={'账号密码登录'} />
-                        <Tabs.TabPane key={'phone'} tab={'手机号登录'} />
+                        <Tabs.TabPane key={'account'} tab={'Email'} />
+                        <Tabs.TabPane key={'phone'} tab={'Số điện thoại'} />
                     </Tabs>
                     {loginType === 'account' && (
                         <>
@@ -64,11 +77,11 @@ const LoginUI = () => {
                                     size: 'large',
                                     prefix: <UserOutlined className={'prefixIcon'} />,
                                 }}
-                                placeholder={'用户名: admin or user'}
+                                placeholder={'Email: admin or user'}
                                 rules={[
                                     {
                                         required: true,
-                                        message: '请输入用户名!',
+                                        message: 'Vui lòng nhập Email của bạn!',
                                     },
                                 ]}
                             />
@@ -93,27 +106,27 @@ const LoginUI = () => {
                                         if (status === 'pass') {
                                             return (
                                                 <div style={{ color: token.colorWarning }}>
-                                                    强度：中
+                                                    Độ mạnh: Trung bình
                                                 </div>
                                             );
                                         }
                                         if (status === 'ok') {
                                             return (
                                                 <div style={{ color: token.colorSuccess }}>
-                                                    强度：强
+                                                    Độ mạnh: Mạnh
                                                 </div>
                                             );
                                         }
                                         return (
-                                            <div style={{ color: token.colorError }}>强度：弱</div>
+                                            <div style={{ color: token.colorError }}>Độ mạnh: Yếu</div>
                                         );
                                     },
                                 }}
-                                placeholder={'密码: ant.design'}
+                                placeholder={'Mật khẩu: ant.design'}
                                 rules={[
                                     {
                                         required: true,
-                                        message: '请输入密码！',
+                                        message: 'Vui lòng nhập mật khẩu！',
                                     },
                                 ]}
                             />
@@ -127,15 +140,15 @@ const LoginUI = () => {
                                     prefix: <MobileOutlined className={'prefixIcon'} />,
                                 }}
                                 name="mobile"
-                                placeholder={'手机号'}
+                                placeholder={'Số điện thoại'}
                                 rules={[
                                     {
                                         required: true,
-                                        message: '请输入手机号！',
+                                        message: 'Vui lòng nhập số điện thoại của bạn！',
                                     },
                                     {
-                                        pattern: /^1\d{10}$/,
-                                        message: '手机号格式错误！',
+                                        pattern: /^0\d{10}$/,
+                                        message: 'Định dạng số điện thoại di động không chính xác！',
                                     },
                                 ]}
                             />
@@ -147,22 +160,22 @@ const LoginUI = () => {
                                 captchaProps={{
                                     size: 'large',
                                 }}
-                                placeholder={'请输入验证码'}
+                                placeholder={'Vui lòng nhập mã xác minh'}
                                 captchaTextRender={(timing, count) => {
                                     if (timing) {
-                                        return `${count} ${'获取验证码'}`;
+                                        return `${count} ${'Nhận mã xác minh'}`;
                                     }
-                                    return '获取验证码';
+                                    return 'Nhận mã xác minh';
                                 }}
                                 name="captcha"
                                 rules={[
                                     {
                                         required: true,
-                                        message: '请输入验证码！',
+                                        message: 'Vui lòng nhập mã xác minh！',
                                     },
                                 ]}
                                 onGetCaptcha={async () => {
-                                    message.success('获取验证码成功！验证码为：1234');
+                                    message.success('Đã nhận được mã xác minh thành công! Mã xác minh là：1234');
                                 }}
                             />
                         </>
@@ -173,14 +186,14 @@ const LoginUI = () => {
                         }}
                     >
                         <ProFormCheckbox noStyle name="autoLogin">
-                            自动登录
+                            Đăng nhập tự động
                         </ProFormCheckbox>
                         <a
                             style={{
                                 float: 'right',
                             }}
                         >
-                            忘记密码
+                            Quên mật khẩu
                         </a>
                     </div>
                 </LoginForm>
